af965b93758a7c1219442b3a63315d4ff6aed54c
iDanishChaturvedi <146645785+iDanishChaturvedi@users.noreply.github.com>
2025-04-21 03:57:28 +0530

    Update arduino_robot.ino

--- arduino_robot/arduino_robot.ino
+++ arduino_robot/arduino_robot.ino
@@ -1,59 +1,72 @@
-#define in1 3
-#define in2 5
-#define in3 6
-#define in4 11
-int command; 
-int Speed = 204; // Fixed speed (0-255)
+// Pin Definitions
+int IN1 = 3;
+int IN2 = 5;
+int IN3 = 6;
+int IN4 = 11;
+
+int buzzer =9;
+char command; // For incoming Bluetooth command
 
 void setup() {
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
-  Serial.begin(9600); // HC-05 default baud rate
+  // Set motor pins as output
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  // Start serial for Bluetooth communication
+  digitalWrite(buzzer, LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
+  if (Serial.available()) {
     command = Serial.read();
-    Serial.print("Received: "); // Print the received command
-    Serial.println(command);    // (e.g., "F", "B", etc.)
-    Stop(); // Stop before new command
-    
-    switch(command) {
-      case 'F': forward(); break;
-      case 'B': back(); break;
-      case 'L': left(); break;
-      case 'R': right(); break;
-      case 'S': Stop(); break;
+    Serial.println(command);
+
+    switch (command) {
+      case 'F': // Forward
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+        break;
+
+      case 'B': // Backward
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        break;
+
+      case 'L': // Left
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+        break;
+
+      case 'R': // Right
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        break;
+
+      case 'S': // Stop
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, LOW);
+        break;
+
+       case 'Y':  // Horn (Buzzer)
+        digitalWrite(buzzer, HIGH);  // Turn buzzer on
+        delay(500);                  // Buzz for half a second
+        digitalWrite(buzzer, LOW);   // Turn buzzer off
+        break;
+
     }
   }
-}
-
-// Movement functions
-void forward() {
-  analogWrite(in1, Speed);
-  analogWrite(in3, Speed);
-}
-
-void back() {
-  analogWrite(in2, Speed);
-  analogWrite(in4, Speed);
-}
-
-void left() {
-  analogWrite(in3, Speed);
-  analogWrite(in2, Speed);
-}
-
-void right() {
-  analogWrite(in4, Speed);
-  analogWrite(in1, Speed);
-}
-
-void Stop() {
-  analogWrite(in1, 0);
-  analogWrite(in2, 0);
-  analogWrite(in3, 0);
-  analogWrite(in4, 0);
 }
